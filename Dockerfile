761d0620436a171828bc18edf29f96197f4d367c
JeanYoung Hong <jeandelugedev@gmail.com>
2023-10-13 13:23:32 +0900

    feat: server init

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ WORKDIR /app
 COPY . .
 RUN pip install -r requirements.txt
 
-EXPOSE 3000
+CMD ["flask", "run", "--host","0.0.0.0"]
--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/hello')
+@app.route('/')
 def hello():
     return 'Hello World!'
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port="5000")
